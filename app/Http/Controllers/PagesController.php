88a414df6f30022b7c6ffdb2ab6a6d77d21e855f
solongo3 <115098880+solongo3@users.noreply.github.com>
2023-09-13 00:35:32 +0800

    first commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index() {
+        $title = 'Welcome to my project';
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about() {
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
+    }
+    public function services() {
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web design', 'programming', '290']
+        );
+        return view('pages.services')->with($data);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\PagesController@index');
+Route::get('/about', 'App\Http\Controllers\PagesController@about');
+Route::get('/services', 'App\Http\Controllers\PagesController@services');
+// Route::get('/hello', function () {
+//     return '<h1> hello laravel </h1>';
+// });
+
+
+// Route::get('/about', function () {
+//     return view('pages.about');
+// });
+
+// Route::get('/users/{id}/{name}', function ($id, $name) {
+//     return 'This is user: '.$id.' and this is name: '.$name;
+// });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
